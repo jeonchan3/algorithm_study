df1a6a0943c5ad89df04003f22452020f9f0e26d
jeonchan3 <jeonchan3@naver.com>
2026-03-31 21:25:54 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 56712 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,3 +1,4 @@
+"""
 N = int(input())
 if N == 1:
     print(1)
@@ -8,3 +9,30 @@ else:
         else:
             print(2**i - 2 * (2**i - N))
             break
+"""
+"""
+2    2
+3    2
+4    4
+5    2
+6    4
+7    6
+8    8
+9    2
+10   4
+11   6
+12   8
+13   10
+14   12
+15   14
+16   16
+"""
+
+N = int(input())
+c = list(range(1, N+1))
+f = 0
+while f < len(c) - 1:
+    f += 1
+    c.append(c[f])
+    f += 1
+print(c[f])
